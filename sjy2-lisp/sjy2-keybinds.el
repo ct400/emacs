5703e17d2ec3b68a22313803086258687b3a1ec6
ct400 <centzon@protonmail.com>
2022-12-29 10:49:56 +0000

    Just an excuse to make another magit commit
    It just seems so less intuitive to me than the command line.

--- sjy2-lisp/sjy2-keybinds.el
+++ sjy2-lisp/sjy2-keybinds.el
@@ -48,8 +48,7 @@
 ;; also check out... https://github.com/Kungsgeten/selected.el
 
 (defadvice kill-region (before unix-werase activate compile)
-  "When called interactively with no active region, delete a single word
-   backwards instead."
+  "When called interactively with no active region, delete a single word backwards instead."
    (interactive
      (if mark-active (list (region-beginning) (region-end))
        (list (save-excursion (backward-word 1) (point)) (point)))))
