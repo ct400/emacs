13eaa9805bf5b0cfa9aa63eaf00c04e5ff67a0fc
ct400 <centzon@protonmail.com>
2023-01-03 16:45:34 +0000

    Replace emacs-surround with embrace

--- sjy2-lisp/sjy2-keybinds.el
+++ sjy2-lisp/sjy2-keybinds.el
@@ -34,14 +34,15 @@
 (global-set-key (kbd "M-<up>")      'ew/move-line-up)
 (global-set-key (kbd "M-<down>")    'ew/move-line-down)
 
-(global-set-key (kbd "C-q")      'emacs-surround)
+;;(global-set-key (kbd "C-q")      'emacs-surround)
+;; replaced with embrace
 (global-set-key (kbd "M-=")      'er/expand-region)
 (global-set-key (kbd "M--")      'er/contract-region)
 ;; C-, == flyspell next
 ;; C-. == flyspell correct
 
 (global-set-key (kbd "C-x e")   'end-of-buffer) ;; kmacro-end-and-call-macro
-(global-set-key (kbd "C-x a")   'begining-of-buffer) ;;
+(global-set-key (kbd "C-x a")   'beginning-of-buffer) ;;
 
 ;; Steve Yegge had C-w for bacward-word and C-x k for kill-region since C-w is consistent with terminal usage and prevents accidental killing of an overly large block if mark is set some way away.
 ;; THIS GEM uses kill-region if there is a selection and backwards-word otherwise. GENIUS!!!
@@ -53,30 +54,6 @@
      (if mark-active (list (region-beginning) (region-end))
        (list (save-excursion (backward-word 1) (point)) (point)))))
 
-;;;;;;;; some kb shortcuts 'cause I am a stupid with little brain
-;; M-; -- comment-dwim
-;; for 'following-line' multiple cursors
-;; C-x <space>... select lines...  C-X rt
-;; C-M <left>|<right>... beginning|end of sexp
-;; M-b M-d == backward-word kill-word == kill whole word.
-;; M-d on its own just kills to end of word
-;; "_" is not a \w!
-;;C-<spc> CS-a C-q " <ret><ret> == select to line start and wrap in quotes
-;; C-S <backspace> kill-whole-line
-;; C-x C-u -- Convert region to upper case
-;; M-q is fill-paragraph. Make a M-S-q  for unfill-paragraph?
-
-;; C-x 4 f -- find-file-other-window
-;; C-x 4 d -- dired-other-window
-;; C-x 4 j -- dired-jump-other-window
-;; enlarge-winwow-horizontally: C-u 10 C-x } 
-;; balance-windows: C-x +
-
-;; C-x u == visualise undoo-tree-visualize
-;; C-p/n == up/down tree; d == diff; t == timestamps; q == quit
-
-;; C-M- f/b == move within sexp
-;; C-x <tab> <l/r arrow> == deindent/indent sexp block below
 
 (provide 'sjy2-keybinds)
 
